3c7290ee08bb05e0af891aa41e1f9048526a4d8f
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-04-05 13:45:54 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*Interface is much like a parent class 
+ Interface = define a "Contract"that all the classes inherating from should follow
+ An interface declare "what a class should have"
+an inheriting class defines "How it should do it"
+
+Some of the benifits of using interface are Security+Multiple inheritance + "Plug and play"
+ */
+
+using System;
+namespace learninginterface
+{
+    interface IShape
+    {
+        int Area(int a, int b);
+        int Perimeter(int a, int b);
+    }
+    public class Square1 : IShape
+    {
+        public int Area(int a, int b)
+        {
+            return a*b;
+        }
+        public int Perimeter(int a,int b)
+        {
+            return 4*a;
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public int Area(int a,int b)
+        {
+            return a*b;
+        }
+        public int Perimeter (int a,int b)
+        {
+            return 2*(a*b);
+        }
+    }
+    public class  static void Main()
+    {
+        Square1 square = new Square1();
+        
+        Console.WriteLine("");
+    }
+}
